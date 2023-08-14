63352c8cea37d0d9a881a829a5de07dbe083a85d
shang <vuvu0130@gmail.com>
2023-08-14 15:02:38 +0800

    build mode

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //開發 或 產品
+  base:process.env.Node_ENV ==="production"?"/react-gh-page-sample/":"/",
   plugins: [react()],
 })
