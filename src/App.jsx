fe692cdb1e78cb343ca8100464ea3160aceb577e
shang <vuvu0130@gmail.com>
2023-08-15 10:47:01 +0800

    change base

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import drinkItems from "./data"
 
+ //修改drinkItem，加上Edit的屬性
+ drinkItems.forEach((item)=>{item.edit = false});
+
+
 function App() {
-  
+ 
+  //建立state
   const [drinks, setDrinks] = useState(drinkItems);
+  
   
   //---Level 2 挑戰 :修改存貨
   function handleStock(btn,e,id){
@@ -28,6 +34,29 @@ function App() {
     }
   }
   //----- Level 3 挑戰 修改名稱
+//編輯按鈕
+function handleEdit(e){
+  const id = parseInt( e.target.value);
+
+  
+     //找出drinks中id與點擊的id匹配的項目，將其name改成新輸入的NewName
+     //回傳修改過的drinks物件陣列
+     const newDrinks =  drinks.map(
+      (item)=>{
+        if(item.id==id){
+          item.edit = !item.edit; //變成相反的boolean
+         
+          return item;
+        }else{
+          return item;
+        }
+      }
+     );
+     console.log(newDrinks[0]);
+     setDrinks(newDrinks);
+}
+
+
   function handleName(e){
     //取得點擊項目的id與輸入的新值
     const id = parseInt(e.target.name);//將id轉成數字，以和drinks中項目的id匹配
@@ -65,8 +94,12 @@ function App() {
       (item)=>(
         <tr key={item.id}>
       <td>
-        <input type="text" value={item.name} name={item.id} 
-         onChange={(e)=>handleName(e)}/>
+       
+      {item.edit?<input type="text" value={item.name} name={item.id}  
+         onChange={(e)=>handleName(e)}/>:<input type="text" value={item.name} name={item.id}  disabled
+         onChange={(e)=>handleName(e)}/>}
+        
+         <button value={item.id}  onClick={(e)=>{handleEdit(e)}} >{item.edit?"完成":"編輯"}</button>
         </td>
       <td><small>{item.description}</small></td>
       <td>{item.price}</td>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //開發 或 產品
-  base:process.env.Node_ENV ==="production"?"/react-gh-page-sample/":"/",
+  base:process.env.Node_ENV ==="production"?"/react-week1-LEVEL2-3/":"/",
   plugins: [react()],
 })
